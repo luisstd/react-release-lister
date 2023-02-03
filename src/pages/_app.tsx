19578ee877103f01f1d51ae72a43dace8ccb8ff2
Luis Steidle <luis@steidle.me>
2023-02-03 22:58:16 +0100

    feat: add light/dark mode functionality

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,34 @@
-import { MantineProvider } from '@mantine/core'
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core'
+import { useHotkeys, useLocalStorage } from '@mantine/hooks'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'mantine-color-scheme',
+    defaultValue: 'light',
+    getInitialValueInEffect: true,
+  })
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
+
+  useHotkeys([['mod+J', () => toggleColorScheme()]])
+
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: colorScheme,
+          fontFamily: 'Satoshi, sans-serif',
+          headings: {
+            fontFamily: 'Cabinet Grotesk, sans-serif',
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </ColorSchemeProvider>
   )
 }
